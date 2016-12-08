2026a60022f80044d7c800a25f2f970861e1e8a0
Josh Edwards <jedwards@rxsavingssolutions.com>
2016-12-08 12:06:02 -0600

    fixed pathing

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ switch (program.generate) {
 }
 
 function createAsset(type, fileName, fileTemplate, specTemplate){
-    const assetPath = appRoot + '/' + type + FOLDER;
+    const assetPath = appRoot + '/src/' + type + FOLDER;
 
     try {
         fs.mkdirSync(assetPath);
